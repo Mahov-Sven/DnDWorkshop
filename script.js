d18e1331f96c1f079d753567cd348e5dfbf516df
FireEmber <empenn54@gmail.com>
2017-07-27 20:01:26 -0500

    Implemented Menu Button and part of Menu
    Animations working.

--- script.js
+++ script.js
@@ -2,7 +2,73 @@ $(document).ready(function () {
 	
 	// ------------- MAIN JAVASCRIPT STUFF -------------
 	
-	var showDice = false;
+	var render = false;
+	
+	var currentTab = 0;
+	/*
+	 * TAB IDs
+	 * 0 => NULL
+	 * 1 => Dice Button
+	 * 2 => Menu Button
+	 *
+	 */
+	
+	function updateTabs(newTab){
+		
+		disableTab(currentTab);
+		
+		if(newTab !== currentTab){
+			enableTab(newTab);
+			currentTab = newTab;
+		}else{
+			currentTab = 0;
+		}
+	}
+	
+	function disableTab(tab){
+		switch(tab){
+			case 0:
+				break;
+				
+			case 1:
+				$("#CANVAS_OVERLAY").addClass("hide");
+				$("#BUTTON_DICE").removeClass("BUTTON_DICE_SHOW");
+				render = false;
+				break;
+				
+			case 2:
+				$("#BUTTON_MENU").removeClass("BUTTON_MENU_SHOW");
+				$("#MENU_ANIM_BAR_T").stop().removeClass("RotateDown45").animate({width: "50%"});
+				$("#MENU_ANIM_BAR_M").stop().removeClass("Fade");
+				$("#MENU_ANIM_BAR_B").stop().removeClass("RotateUp45").animate({width: "50%"});
+				$("#MAIN_CONTENT_CONTAINER").removeClass("UnfocusContentContainer");
+				$("#MENU").stop(true,true).slideUp();
+				break;
+		}
+	}
+	
+	function enableTab(tab){
+		switch(tab){
+			case 0:
+				break;
+				
+			case 1:
+				$("#CANVAS_OVERLAY").removeClass("hide");
+				$("#BUTTON_DICE").addClass("BUTTON_DICE_SHOW");
+				scene.onSimulationResume();
+				render = true;
+				break;
+				
+			case 2:
+				$("#BUTTON_MENU").addClass("BUTTON_MENU_SHOW");
+				$("#MENU_ANIM_BAR_T").stop().addClass("RotateDown45").animate({width: "70.7%"});
+				$("#MENU_ANIM_BAR_M").stop().addClass("Fade");
+				$("#MENU_ANIM_BAR_B").stop().addClass("RotateUp45").animate({width: "70.7%"});
+				$("#MAIN_CONTENT_CONTAINER").addClass("UnfocusContentContainer");
+				$("#MENU").stop(true,true).slideDown();
+				break;
+		}
+	}
 	
 	function getRenderingWidth(){
 		return $("#CANVAS_OVERLAY").width();
@@ -25,7 +91,7 @@ $(document).ready(function () {
 	
 	var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true});
 	renderer.setSize( getRenderingWidth(), getRenderingHeight() );
-	renderer.setClearColor( 0x000000, 0.5 )
+	renderer.setClearColor( 0x000000, 0.0 )
 	renderer.domElement.style.position = "absolute";
 	document.getElementById("CANVAS_OVERLAY").appendChild( renderer.domElement );
 
@@ -50,7 +116,7 @@ $(document).ready(function () {
 
 	function animate() {
 		requestAnimationFrame( animate );
-		if(showDice){
+		if(render){
 			scene.simulate();
 			renderer.render( scene, camera );
 		}
@@ -68,22 +134,13 @@ $(document).ready(function () {
 		renderer.setSize(getRenderingWidth(), getRenderingHeight());
 	});
 	
-	$("#BUTTON_DICE").mouseup(function(){
-		$("#CANVAS_OVERLAY").toggleClass("hide");
-		$("#BUTTON_DICE").toggleClass("BUTTON_DICE_SHOW");
-		showDice = !showDice;
-		
-		if(showDice){
-			scene.onSimulationResume();
-		}
-	});
-	
-	//Function executed when clicking on the menuBox.
-	$("#menuBox").toggle(function(){
-		showMenu();
-	}, function(){
-		hideMenu();
-	});
-	
 	// ------------- END OF JQUERY STUFF -------------
+	
+	$("#BUTTON_DICE").mouseup(function(){
+		updateTabs(1);
+	});
+	
+	$("#BUTTON_MENU").mouseup(function(){
+		updateTabs(2);
+	});
 });
