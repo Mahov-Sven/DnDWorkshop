66c68a5efadf1378112e79e47b22e659fab9468c
FireEmber <empenn54@gmail.com>
2017-08-04 16:08:49 -0500

    Added basic functionality for LOADER

--- files/scripts/loader.js
+++ files/scripts/loader.js
@@ -1,3 +1,83 @@
 var LOADER = {
-	
+	loadHTML: null,
+	loadMenuFile: null,
+	loadPlayerFile: null,
+	loadRuleSet: null,
+	loadSkillSet: null,
+	loadSpellbook: null,
+	loadInventory: null,
+	loadItemShop: null,
+	loadEncounter: null,
+}
+
+LOADER.loadHTML = function(fileLocation, requireExtension){
+	return null;
+}
+
+LOADER.loadHTML = function(fileLocation){
+	return loadHTML(fileLocation, false);
+}
+
+LOADER.loadMenuFile = function(fileLocation, requireExtension){
+	return null;
+}
+
+LOADER.loadMenuFile = function(fileLocation){
+	return loadMenuFile(fileLocation, false);
+}
+
+LOADER.loadPlayerFile = function(fileLocation, requireExtension){
+	return null;
+}
+
+LOADER.loadPlayerFile = function(fileLocation){
+	return loadPlayerFile(fileLocation, false);
+}
+
+LOADER.loadRuleSet = function(fileLocation, requireExtension){
+	return null;
+}
+
+LOADER.loadRuleSet = function(fileLocation){
+	return loadRuleSet(fileLocation, false);
+}
+
+LOADER.loadSkillSet = function(fileLocation, requireExtension){
+	return null;
+}
+
+LOADER.loadSkillSet = function(fileLocation){
+	return loadSkillSet(fileLocation, false);
+}
+
+LOADER.loadSpellbook = function(fileLocation, requireExtension){
+	return null;
+}
+
+LOADER.loadSpellbook = function(fileLocation){
+	return loadSpellbook(fileLocation, false);
+}
+
+LOADER.loadInventory = function(fileLocation, requireExtension){
+	return null;
+}
+
+LOADER.loadInventory = function(fileLocation){
+	return loadInventory(fileLocation, false);
+}
+
+LOADER.loadItemShop = function(fileLocation, requireExtension){
+	return null;
+}
+
+LOADER.loadItemShop = function(fileLocation){
+	return loadItemShop(fileLocation, false);
+}
+
+LOADER.loadEncounter = function(fileLocation, requireExtension){
+	return null;
+}
+
+LOADER.loadEncounter = function(fileLocation){
+	return loadEncounter(fileLocation, false);
 }
