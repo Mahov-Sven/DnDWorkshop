599fdbca271ca46c9f345b95af2ce1a2bb989b5e
FireEmber <empenn54@gmail.com>
2017-08-07 19:52:32 -0500

    Continued work on loading the menu

--- files/scripts/loader.js
+++ files/scripts/loader.js
@@ -1,8 +1,17 @@
 var htmlExt = ".dndhtml";
 var menuFileExt = ".dndmf";
 var srtRegex = /[\n\r\s\t]+/g;
+var menuIdPrepend = "Menu-Id-";
 
 function Loader(){}
+
+/* ------------ GENERAL FUNCTIONS ------------ */
+
+function createDiv(){
+	return document.createElement("div");
+}
+
+/* ------------ GENERAL FILE LOADING ------------ */
 
 Loader.prototype.loadFile = function(fileLocation, callback){
 	$.get(fileLocation, function(file){
@@ -10,19 +19,101 @@ Loader.prototype.loadFile = function(fileLocation, callback){
 	});
 }
 
+/* ------------ HTML FILE LOADING ------------ */
+
 Loader.prototype.loadHTML = function(fileLocation, callback){
 	this.loadFile(fileLocation + htmlExt, function(html){
 		callback(html);
 	});
 }
 
+/* ------------ MENU FILE LOADING ------------ */
+
+function createBaseMenu(){
+	var MenuContainer = createDiv();
+	MenuContainer.classList.add("MenuContainer");
+	
+	var MENU = createDiv();
+	MENU.style.display = "none";
+	MENU.id = "MENU";
+	
+	var menu = createDiv();
+	menu.classList.add("Menu");
+	menu.id = menuIdPrepend + "";
+	
+	MENU.appendChild(menu);
+	MenuContainer.appendChild(MENU);
+	
+	console.log(MenuContainer);
+	return MenuContainer;
+}
+
+function createSubMenu(MenuPath){
+	var SubMenuContainer = createDiv();
+	SubMenuContainer.classList.add("SubMenuContainer");
+	
+	var MenuContainer = createDiv();
+	MenuContainer.classList.add("MenuContainer");
+	
+	var menu = createDiv();
+	menu.classList.add("Menu");
+	menu.id = menuIdPrepend + MenuPath;
+	
+	MenuContainer.appendChild(menu);
+	SubMenuContainer.appendChild(MenuContainer);
+	
+	return SubMenuContainer;
+}
+
+function createMenuSelection(MenuName, MenuPath){
+	var MenuSelection = createDiv();
+	MenuSelection.classList.add("MenuSelection");
+	
+	var MenuSelectionText = createDiv();
+	MenuSelectionText.classList.add("MenuSelectionText");
+	MenuSelectionText.innerHTML = MenuName;
+	
+	var MenuSelectionImg = createDiv();
+	MenuSelectionImg.classList.add("MenuSelectionImg");
+	MenuSelectionImg.style.display = "none";
+	
+	var MenuSelectionSubMenu = createSubMenu(MenuPath);
+	
+	MenuSelection.appendChild(MenuSelectionText);
+	MenuSelection.appendChild(MenuSelectionImg);
+	MenuSelection.appendChild(MenuSelectionSubMenu);
+	
+	console.log(MenuSelection);
+	
+	return MenuSelection;
+}
+
+function attatchMenu(parentName, menuName){
+	
+}
+
 Loader.prototype.loadMenuFile = function(fileLocation, callback){
 	this.loadFile(fileLocation + menuFileExt, function(file){
 		var trimmed = file.replace(srtRegex, '');
 		var menuArray = trimmed.split(";").filter(function(token) {return token.length != 0});
+		var html = "";
+		
+		var base = createBaseMenu();
+		var subm = createMenuSelection("TEST", "MAIN.SUB");
+		
+		for(var i = 0; i < menuArray.length; i++){
+			var submenuData = menuArray[i].split(":");
+			var submenu = submenuData[0].split(".");
+			var data = submenuData[1].substring(1, submenuData[1].length - 1).split(",");
+			console.log(submenu);
+			console.log(data);
+		}
+		
 		callback(menuArray);
 	});
 }
+
+/* ------------ PLAYER FILE LOADING ------------ */
 
 Loader.prototype.loadPlayerFile = function(fileLocation, requireExtension){
 	return null;
