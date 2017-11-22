835744bfda3b36daf636264c198183a16b7a5495
Ethan Penn <empenn54@gmail.com>
2017-11-22 12:07:46 -0600

    Created Parser Class

--- files/res/rules/rulebook.js
+++ files/res/rules/rulebook.js
@@ -1,213 +1,100 @@
 /* PARSING */
-var deleteRegex = /(\/\*.*\*\/)|(\/\/.*)|[\n\r\s\t]/g;
-
-class PostEvaluateFunction {
-	constructor(functionName, functionArgs){
-		this.functionName = functionName;
-		this.functionArgs = functionArgs;
-	}
-}
-
-class PostEvaluateObject {
-	constructor(objectName, objectArgs){
-		this.objectName = objectName;
-		this.objectArgs = objectArgs;
-	}
-}
+const deleteRegex = /(\/\*[^*](.|\n)*?\*\/)|(\/\/.*)|[\n\r\s\t]/g;
+const wordSeparators = [' ', '.', '{', '}','[', ']', '\'', ';', '(', ')', ':', ',', '\n', '\t'];
 
 class Rulebook {
 	
 	static parseRulebook(file){
 		
-		let result = file.replace(deleteRegex, '');
-		let functions = [];
-		while(result.length !== 0){
-			let indexLastBracket = 0;
-			
-			let currentScope = 0;
-			while(true){
-				const char = result.charAt(indexLastBracket);
-				if(char === 0) throw Error("The file did not have the correct ending.");
-				if(char === '{'){
-					currentScope++;
-				}else if(char === '}'){
-					currentScope--;
-					if(currentScope === 0){
-						indexLastBracket++;
-						break;
-					}
-				}
-				indexLastBracket++;
-			}
-			
-			functions.push(result.substr(0, indexLastBracket));
-			result = result.substr(indexLastBracket, result.length);
-		}
+		let text = file.replace(deleteRegex, '');
 		
-		for(let i = 0; i < functions.length; i++){
-			let functionParts = [];
-			let functionText = functions[i];
-			let charIndex = 0;
-			
-			let currentScope = 0;
-			while(true){
-				const char = functionText.charAt(charIndex);
-				if(char === 0) throw Error("There was an error parsing an object's name.");
-				if(char === '<' || char === '('){
-					break;
-				}
-				charIndex++;
-			}
-			
-			functionParts[0] = functionText.substr(0, charIndex);
-			functionText = functionText.substr(charIndex, functionText.length);
-			charIndex = 0;
-			
-			functionParts[1] = "";
-			
-			if(functionText.charAt(charIndex) === '<'){
-				while(true){
-					const char = functionText.charAt(charIndex);
-					if(char === 0) throw Error("There was an error parsing an object's super classes.");
-					if(char === '>'){
-						break;
-					}
-					charIndex++;
-				}
-				
-				functionParts[1] = functionText.substr(1, charIndex - 1);
-				functionText = functionText.substr(charIndex + 1, functionText.length);
-				charIndex = 0;
-			}
-			
-			functionParts[2] = "";
-			
-			if(functionText.charAt(charIndex) === '('){
-				while(true){
-					const char = functionText.charAt(charIndex);
-					if(char === 0) throw Error("There was an error parsing an object's parameters.");
-					if(char === ')'){
-						break;
-					}
-					charIndex++;
-				}
-				
-				functionParts[2] = functionText.substr(1, charIndex - 1);
-				functionText = functionText.substr(charIndex + 1, functionText.length);
-				charIndex = 0;
-			}
-			
-			functionParts[3] = [];
-			
-			if(functionText.charAt(0) !== '{' && functionText.charAt(functionText.length - 1) != '}'){
-				throw Error("The inner part of the funciton did not close/open with braces properly");
-			}
-			
-			functionText = functionText.substr(1, functionText.length - 2);
-			
-			while(functionText !== ""){
-				const char = functionText.charAt(charIndex);
-				if(char === 0) throw Error("There was an error parsing an object's contents.");
-				if(char === '(' || char === '{'){
-					currentScope++;
-				}else if(char === ')' || char === '}'){
-					currentScope--;
-				}else if(char === ';' && currentScope === 0){
-					functionParts[3].push(functionText.substr(0, charIndex));
-					functionText = functionText.substr(charIndex + 1, functionText.length);
-					charIndex = 0;
-					continue;
-				}
-				charIndex++;
-			}
-			
-			for(let j = 0; j < functionParts[3].length; j++){
-				functionText = functionParts[3][j];
-				let functionInside = [];
-				if(functionText.substr(0, 5) !== "this.") throw Error("'this' was not used when declaring an object's property.");
-			
-				functionText = functionText.substr(5, functionText.length);
-				charIndex = 0;
-				while(true){
-					const char = functionText.charAt(charIndex);
-					if(char === '='){
-						break;
-					}
-					charIndex++;
-				}
-				functionInside[0] = functionText.substr(0, charIndex);
-				functionInside[1] = functionText.substr(charIndex + 1, functionText.length);
-				
-				functionParts[3][j] = functionInside;
-			}
-			
-			for(let j = 0; j < functionParts[3].length; j++){
-				const functionPartContent = functionParts[3][j][1];
-				const foundIndex = functionPartContent.indexOf("(")
-				let varName = functionPartContent.substring(0, foundIndex === -1 ? functionPartContent.length : foundIndex);
-				const fromThis = varName.substring(0, varName.indexOf(".")) === "this";
-				const isString = functionPartContent.charAt(0) === '"' && functionPartContent.charAt(functionPartContent.length - 2) === '"';
-				const isArray = functionPartContent.charAt(0) === '[' && functionPartContent.charAt(functionPartContent.length - 2) === ']';
-				varName = varName.substring(varName.indexOf(".") + 1);
-				const functionName = functionParts[0] + "." + functionParts[3][j][0];
-				const functionString = functionPartContent.substring(foundIndex + 1, functionPartContent.lastIndexOf(")"));
-				const functionStringArray = functionString.split(",");
-				console.log(functionString);
-				switch(varName){
-				case "null":
-					Rulebook.__variables[functionName] = null;
-					break;
-				case "Function":
-					const functionParams = functionString.substring(functionString.indexOf("(") + 1, functionString.indexOf(")"));
-					const functionContent = functionString.substring(functionString.indexOf("{") + 1, functionString.lastIndexOf("}"));
-					/* [The function Name (what 'this' refers to), The function's parameters as an array, the function's content as a string] */
-					const functionArray = [functionParts[0], functionParams.split(","), functionContent];
-					
-					Rulebook.__functions[functionName] = functionArray;
-					break;
-				default:
-					if(isString){
-						Rulebook.__variables[functionName] = varName;
-					}if(isArray){
-						// TODO
-					}else if(fromThis){
-						Rulebook.__variables[functionName] = new PostEvaluateFunction(varName, functionStringArray);
-					}else{
-						Rulebook.__variables[functionName] = new PostEvaluateObject(varName, functionStringArray);
-					}
-					break;
-				}
-			}
-			
-			//console.log(functionText);
-			
-			console.log(functionParts);
-		}
 		
-		console.log(Rulebook.__functions);
-		console.log(Rulebook.__variables);
 	}
 }
 
-Rulebook.__functions = [];
-Rulebook.__variables = [];
-
-Rulebook.prototype.BIND_ELEMENT = function(elem, valueName){
-	this.elem = elem;
-	this.valueName = valueName;
-	this.value = function(){return RULEBOOK.FUNCTIONS[this.valueName];};
-	elem.value  = this.value;
-	elem.addEventListener("change", this, false);
+class Parser {
+	constructor(string, wordSeparatorChars, contextStartChars, contextEndChars){
+		this.text = string;
+		this.wordSeparatorChars = new Set(wordSeparatorChars);
+		this.contextStartChars = new Set(contextStartChars);
+		this.contextEndChars = new Set(contextEndChars);
+		this.charPointer = 0;
+		this.currentWord = undefined;
+		this.currentContext = undefined;
+		
+		this._updateWord();
+		//this._updateContext();
+	}
 	
-}
-
-Rulebook.prototype.BIND_ELEMENT.prototype.handleEvent = function(evt){
-	switch(evt.type){
-		case "onValueChange": this.onValueChange(this.elem.value);
+	char(){
+		return this.text.charAt(this.charPointer);
 	}
-}
-
-Rulebook.prototype.BIND_ELEMENT.prototype.onValueChange = function(value){
-	RULEBOOK.FUNCTIONS[this.valueName].setValue(value);
-	this.elem.value = value;
+	
+	_previous(){
+		if(this.charPointer - 1 <= 0) return undefined;
+		this.charPointer--;
+		return this.char();
+	}
+	
+	previous(){
+		this._previous();
+		if(this.currentWord === undefined || this.wordSeparatorChars.has(this.char())) this._updateWord();
+		return this.char();
+	}
+	
+	_next(){
+		if(this.charPointer + 1 >= this.text.length) return undefined;
+		this.charPointer++;
+		return this.char();
+	}
+	
+	next(){
+		this._next();
+		if(this.currentWord === undefined || this.wordSeparatorChars.has(this.char())) this._updateWord();
+		return this.char();
+	}
+	
+	_updateWord(){
+		if(this.wordSeparatorChars.has(this.char())) this.currentWord === undefined;
+		const word = [];
+		
+		let charPointer = this.charPointer;
+		let char = this.text.charAt(charPointer);
+		while(charPointer >= 0 && !this.wordSeparatorChars.has(char)){
+			charPointer--;
+			char = this.text.charAt(charPointer);
+		}
+		if(charPointer === -1) this.currentWord = undefined;
+		
+		charPointer++;
+		char = this.text.charAt(charPointer);
+		while(charPointer < this.text.length && !this.wordSeparatorChars.has(char)){
+			word.push(char);
+			charPointer++;
+			char = this.text.charAt(charPointer);
+		}
+		
+		this.currentWord = word.join('');
+	}
+	
+	word(){
+		return this.currentWord;
+	}
+	
+	nextWord(){
+		let next = this._next();
+		while(next !== undefined && !this.wordSeparatorChars.has(next)){
+			next = this._next();
+		}
+		
+		while(next !== undefined && this.wordSeparatorChars.has(next)){
+			next = this._next();
+		}
+		this._updateWord();
+		return this.word();
+	}
+	
+	nextContext(){
+		
+	}
 }
